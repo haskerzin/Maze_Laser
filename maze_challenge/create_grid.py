146ef00ca4a41127b398ee53c6d32e09bf9bf25f
Gabriel <gabriel.rodrigues.aluno@gmail.com>
2022-12-13 22:08:28 -0300

    Refinamento dos metodos

--- maze_challenge/create_grid.py
+++ maze_challenge/create_grid.py
@@ -1,12 +1,15 @@
 import random, time, os
+import numpy as np
 
 class Grid:
 
     def __init__(self):
         self.size = 12
         self.__laser = (0,1)
+        self.__saida = (11,1)
         self.espelhos = []
         self.__bombas = []
+        self.tipo = 0
 
     
     # Método para gerar os índices da parte superior do grid
@@ -24,20 +27,18 @@ class Grid:
     # Método para definir onde ficará a origem do laser
     def laser(self):
         self.__laser = (0, random.randint(1, self.size - 2))
-        return self.__laser
 
     # Método para definir por onde o laser deve sair
     def saida(self):
-        return (self.size-1, random.randint(1, self.size - 2))
+        self.__saida =  (self.size-1, random.randint(1, self.size - 2))
     
     # Método de colocação das bombas
     def coloca_bombas(self, n_bombas, laser, saida):
+        self.__bombas = []
         while len(self.__bombas) < n_bombas:
             biribinha = (random.randint(1,10), random.randint(1,10))
             if biribinha not in self.__bombas and biribinha != (laser[0] + 1,laser[1]) and biribinha != (saida[0] - 1, saida[1]):
                 self.__bombas.append(biribinha)
-
-        return self.__bombas
 
     def checa_inteiro(self, valor):
         try:
@@ -46,18 +47,41 @@ class Grid:
         except:
             return False
     
+    # Metodo que verifica se uma posicao dada esta dentro dos limites do grid
+    def verifica_posicao(self, posicao):
+        verifica = 0
+        for p in posicao:
+            if (1 <= p <= self.size - 1) == False:
+                verifica += 1
+        
+        if verifica != 0:
+            return False
+        else:
+            return True
+
+    ########################## Metodos dos espelhos ################################################################################################################################
     
     # Metodo de escolher o tipo de espelho
     def tipo_espelho(self):
-        print('Digite 1 para espelho \ e 2 para espelho /')
-        tipo = input('Tipo: ')
-        while tipo not in (1,2) or self.checa_inteiro(tipo) == False:
-            print('Valor inválido. Digite 1 para espelho \ e 2 para espelho /')
-            tipo = input('Tipo: ')
+        # print('Digite 1 para espelho \ e 2 para espelho /')
+        tipo = input('Digite 1 para espelho \ e 2 para espelho /: ')
         
-        return int(tipo)
+        verifica = False
 
+        while verifica == False:
+            if self.checa_inteiro(tipo) == True:
+                if int(tipo) in (1,2):
+                    verifica = True
 
+                else:
+                    # print('Valor inválido. Digite 1 para espelho \ e 2 para espelho /')
+                    tipo = input('Tipo: ')
+            else:
+                # print('Valor inválido. Digite 1 para espelho \ e 2 para espelho /')
+                tipo = input('Tipo: ')
+
+        self.tipo = int(tipo)
+    
     # Metodo de receber input da posicao dos espelhos
     def posicao_espelho(self):
         x_espelho = input("Coluna do espelho: ")
@@ -73,29 +97,18 @@ class Grid:
 
         return (x_espelho, y_espelho)
     
-    # Metodo que verifica se uma posicao dada esta dentro dos limites do grid
-    def verifica_posicao(self, posicao):
-        verifica = 0
-        for p in posicao:
-            if (1 <= p <= self.size - 1) == False:
-                verifica += 1
-        
-        if verifica != 0:
-            return False
-        else:
-            return True
-
     # Metodo que adiciona a posicao de um espelho na lista de espelhos
     def espelho(self):
         
         posicao_espelho = self.posicao_espelho()
 
         while (posicao_espelho in self.__bombas) or (self.verifica_posicao(posicao_espelho) == False):
-            print('Posição inválida. Digite novamente.')
+            # print('Posição inválida. Digite novamente.')
             posicao_espelho = self.posicao_espelho()
         
         self.espelhos.append(posicao_espelho)
 
+    ################################################################################################################################################################################
 
     # Método de desenho do caminho do laser
     def raio(self, origem_laser):
@@ -117,7 +130,12 @@ class Grid:
         
         return caminho_laser
 
-
+    # Metodo para definir as posicoes da origem e saida do laser e das bombas antes de chamar a funcao para desenhar a tela
+    def define_aparatos(self):
+        self.laser()
+        self.saida()
+        self.coloca_bombas(5, self.__laser, self.__saida)
+    
     # Método de desenho da grade
     def draw_grid(self):
         
@@ -125,30 +143,37 @@ class Grid:
         wall = ' w '
         ponto = ''
         linha = 1
-        laser = self.laser()
-        saida = self.saida()
+        # laser = self.laser()
+        # saida = self.saida()
         # Essa chamada do método self.bombas() terá que sair do método self.draw_grid() pois
         # após iniciar o jogo a posição das bombas não mudam
-        bombas = self.coloca_bombas(3, laser, saida)
-        caminho_laser = self.raio_novo(laser, bombas)
+        # bombas = self.coloca_bombas(5, laser, saida)
+        caminho_laser = self.raio_novo(self.__laser, self.__bombas)
         self.gera_indices()
         
         for i in range(self.size):
             ponto =''
             for j in range(self.size+1):
                 
-                # Desenhando a origem do laser
-                if (j,i) == laser:
+                # Desenhando a origem do self.__laser
+                if (j,i) == self.__laser:
                     ponto += ' = '
                 
                 # Desenhando a saída 
-                elif (j,i) == saida:
+                elif (j,i) == self.__saida:
                     ponto += '   '
 
                 # Desenhando a bomba
-                elif (j,i) in bombas:
+                elif (j,i) in self.__bombas:
                     ponto += ' B '
                 
+                # Desenhando os espelhos
+                elif (j,i) in self.espelhos:
+                    if self.tipo == 1:
+                        ponto += ' \ '
+                    else:
+                        ponto += ' / '
+
                 # Desenhando o caminho do laser
                 elif (j,i) in caminho_laser:
                     ponto += ' - '
@@ -168,3 +193,22 @@ class Grid:
                         ponto += blank
                 
             print(ponto)
+
+    
+    ###################### Metodo para rodar o jogo #######################################
+
+    def joga(self):
+        jogando = True
+        # Definindo as posicoes de entrada e saida do laser e das bombas
+        self.define_aparatos()
+
+        while jogando == True:
+            # Desenhando o campo
+            self.draw_grid()
+
+            ################## Inputs do jogador ###########################
+            # Selecionando o tipo de espelho
+            self.tipo_espelho()
+
+            # Selecionando a posicao do espelho
+            self.espelho()
